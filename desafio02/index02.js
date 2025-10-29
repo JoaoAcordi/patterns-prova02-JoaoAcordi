c908c818659091b388e6f7f3e87467905e7f5f26
João Pedro Acordi <88681190+JoaoAcordi@users.noreply.github.com>
2025-10-29 22:32:36 +0000

    add

--- desafio02/index02.js
+++ desafio02/index02.js
@@ -1,1 +1,31 @@
-console.log("Hello, World! 002");
+class Message {
+  constructor(text) {
+    this.text = text;
+  }
+
+  getText() {
+    return this.text;
+  }
+}
+
+class MessageDecorator {
+  constructor(message) {
+    this.message = message;
+  }
+
+  getText() {
+    return this.message.getText();
+  }
+}
+
+class PositiveMessageDecorator extends MessageDecorator {
+  getText() {
+    return this.message.getText().replace("horrível", "incrível");
+  }
+}
+
+const msg = new Message("hoje o dia está horrível");
+const positiveMsg = new PositiveMessageDecorator(msg);
+
+console.log(msg.getText());
+console.log(positiveMsg.getText());
